d7781e5041627d6da40142874d66f7a71b3daea9
Gautam <gautam>
2020-09-01 20:45:27 -0500

    Added unzip code and single character search

--- pbi_search.py
+++ pbi_search.py
@@ -1,4 +1,4 @@
-import os, shutil, sys
+import os, shutil, sys, zipfile
 from os import listdir
 
 class Search:
@@ -36,6 +36,58 @@ class Search:
     def capture_input(self):
         text = input('Enter a measure, column, or entity: ')
         return text
+    
+    def unzip(self, target_directory):
+        folder_list = []
+        for file in os.listdir(target_directory):
+            src_file_path = target_directory + '/' + file
+            dest_file_path = src_file_path.replace('.zip','')
+            with zipfile.ZipFile(src_file_path,'r') as zip_ref:
+                zip_ref.extractall(dest_file_path)
+                folder_list.append(dest_file_path)
+        return folder_list
+
+    def search(self):
+        text = self.capture_input()
+        folders = self.unzip(self.target_directory)
+        paths = []
+        for folder in folders:
+            layout_file = folder + '/Report/Layout'
+            layout_text = layout_file + '.txt' 
+            os.rename(layout_file, layout_text)
+            os.chdir(folder + '/Report')
+            print(os.getcwd())
+            
+            '''
+            with open('Layout.txt','r') as f:
+                search_lines = f.readlines()
+            for i, line in enumerate(search_lines):                
+                if text in line:
+                    for l in search_lines[i:i+3:]:
+                        print(l)
+                        paths.append(l)
+            '''
+            
+            search_file = open('Layout.txt', 'r')
+            for i, line in enumerate(search_file):
+                line = line.strip().lower()
+                text = text.strip().lower()
+                print(i, text, len(line), len(text), line.count(text))
+                if line.count(text) > 0:
+                #if text.lower() in line.lower():
+                    if folder not in paths:
+                        paths.append(folder)
+            search_file.close()
+            
+            '''
+            with open('Layout.txt','r') as f:
+                print('Searching ' + text + ' in ' + layout_text)
+                print(any(text in line for line in f))
+                if text in f.read():
+                    paths.append(folder)
+            '''
+        print(paths)
+        return paths
 
 
 
@@ -44,5 +96,6 @@ class Search:
 directory = '/mnt/c/Reports/'
 app = Search(directory)
 app.copy_files()
-print(app.capture_input())
-
+#print(app.capture_input())
+#print(app.unzip(directory + 'target/'))
+print(app.search())
