0fcb0e2470675d11a868b7798daca3ed3fdf5779
Gautam <gautam>
2020-09-08 20:41:00 -0500

    Updated search_file function with encoding type

--- pbi_search.py
+++ pbi_search.py
@@ -1,4 +1,4 @@
-import os, shutil, sys, zipfile
+import os, shutil, sys, zipfile, mmap, pylint, codecs
 from os import listdir
 
 class Search:
@@ -47,7 +47,22 @@ class Search:
                 folder_list.append(dest_file_path)
         return folder_list
 
-    def search(self):
+    
+    def search_file(self, file, text):
+        #with open(file, 'rb') as read_obj:
+        #with open(file, "r", "utf-16") as read_obj:
+        read_obj = codecs.open(file, "r", "utf-16-le")
+        for line in read_obj:
+            line = line.strip().lower().replace('"', '').replace("\\","")
+            text = text.strip().lower()
+            print(text, len(line), len(text), line.count(text),line.find(text),str(text) in str(line))#,str(line))
+            if str(text) in str(line):
+                return True
+        read_obj.close()
+        return False
+
+
+    def search_directory(self):
         text = self.capture_input()
         folders = self.unzip(self.target_directory)
         paths = []
@@ -57,7 +72,19 @@ class Search:
             os.rename(layout_file, layout_text)
             os.chdir(folder + '/Report')
             print(os.getcwd())
-            
+
+            if self.search_file('Layout.txt', text):
+                paths.append(folder)
+            '''
+            with open('Layout.txt', 'r') as read_obj:
+                for line in read_obj:
+                    line = line.strip().lower().replace('"', '').replace("\\","")
+                    text = text.strip().lower()
+                    print(text, len(line), len(text), line.count(text),line.find(text),str(text) in str(line))#,str(line))
+                    if str(text) in str(line):
+                        paths.append(folder)
+            read_obj.close()
+            '''
             '''
             with open('Layout.txt','r') as f:
                 search_lines = f.readlines()
@@ -67,18 +94,24 @@ class Search:
                         print(l)
                         paths.append(l)
             '''
-            
+            '''(i)  data   = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            (ii) result = data.find(search_str)'''
+
+            '''
+            data = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            '''
+            '''
             search_file = open('Layout.txt', 'r')
             for i, line in enumerate(search_file):
                 line = line.strip().lower()
                 text = text.strip().lower()
-                print(i, text, len(line), len(text), line.count(text))
-                if line.count(text) > 0:
+                print(i, text, len(line), len(text), line.count(text),line.find(text),str(text) in str(line))#,str(line))
+                if text in line:
+                    paths.append(folder)
                 #if text.lower() in line.lower():
-                    if folder not in paths:
-                        paths.append(folder)
+                    #if folder not in paths:
             search_file.close()
-            
+            '''
             '''
             with open('Layout.txt','r') as f:
                 print('Searching ' + text + ' in ' + layout_text)
@@ -92,10 +125,17 @@ class Search:
 
 
 
-    
 directory = '/mnt/c/Reports/'
 app = Search(directory)
 app.copy_files()
 #print(app.capture_input())
 #print(app.unzip(directory + 'target/'))
-print(app.search())
+print(app.search_directory())
+
+'''
+test_directory = '/mnt/c/Test/'
+test_app = Search(test_directory)
+test_file='/mnt/c/Test/Layout.txt'
+text = 'Man'
+test_app.search_file(test_file, text)
+'''
